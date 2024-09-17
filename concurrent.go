f449660edbbafb0edfb20b1653ed7d80e4e5f5a9
Piyush Singariya <piyushsingariya@gmail.com>
2024-09-17 08:11:26 +0000

    adding yield based concurrency in relec

--- concurrent.go
+++ concurrent.go
@@ -38,3 +38,47 @@ func Concurrent[T any](ctx context.Context, array []T, concurrency int, execute 
 	// block the execution
 	return executor.Wait()
 }
+
+func ConcurrentC[T any](ctx context.Context, yield <-chan T, concurrency int, execute func(one T) error) error {
+	executor, ctx := errgroup.WithContext(ctx)
+	exit, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	semaphore := make(chan int, concurrency)
+
+	for one := range yield {
+		// schedule an execution
+		executor.Go(func() error {
+			// hold loop till a slot is available
+		hold:
+			for {
+				select {
+				case semaphore <- 1:
+					break hold
+				case <-exit.Done():
+					return nil
+				}
+			}
+
+			// schedule defering to enable an execution slot
+			defer func() {
+				<-semaphore
+			}()
+			return execute(one)
+		})
+	}
+
+	// block the execution
+	return executor.Wait()
+}
+
+func Yield[T any](generate func(channel chan<- T)) <-chan T {
+	channel := make(chan T)
+
+	go func() {
+		defer close(channel)
+		generate(channel)
+	}()
+
+	return channel
+}
